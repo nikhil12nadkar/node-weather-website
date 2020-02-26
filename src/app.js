3cfb48cebe98ee6056900b63e0738e7f45dda458
nikhil_nav <nikhil.nadkar@navituseducation.com>
2020-02-26 16:43:45 +0530

    setup done for application to be run on heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const path      =   require('path')
 const express   =   require('express')
 const hbs       =   require('hbs')
 const app       =   express()
+const port      =   process.env.PORT||3000
 
 console.log(__dirname)
 console.log(__filename)
@@ -101,6 +102,6 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log('server running on port 3000')
+app.listen(port,()=>{
+    console.log('server running on port '+port)
 })
