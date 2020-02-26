49bf9b254bfb92a2353c648e26766f1da1733a39
nikhil_nav <nikhil.nadkar@navituseducation.com>
2020-02-26 12:25:06 +0530

    removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,5 @@
 console.log('client side javascrip has been loaded!!!')
-// fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-//     response.json().then((data)=>{
-//         console.log(data)
-//     })
-// })
+
 
 
 
